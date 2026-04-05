c75ba546e3c5711a5adeebf1e4b9b9b30a6e6233
Flawx <190200856+Matxe24@users.noreply.github.com>
2026-04-05 17:26:24 +0200

    Improve subprocess handling and add platform check
    Added shell=True and check=True to subprocess.run for better error handling. Included an else clause to inform users on non-Windows platforms.

--- py_conf.py
+++ py_conf.py
@@ -3,10 +3,14 @@ import os
 
 if os.name == 'nt':
     try:
-        sub.run('py -m pip install -r requirements.txt')
-        input()
+        sub.run('py -m pip install -r requirements.txt', shell=True, check=True)
         print("Configuration finished.")
-    except any:
+        input()
+    except Exception:
         print("Configuration failed, try running 'py -m pip install -r requirements.txt' in Powershell.")
         input()
         exit()
+else:
+    print("This is only for windows. Check out the README or the github page on https://github.com/Matxe24/tesint for the installation for your specific platform")
+    input()
+    exit()
