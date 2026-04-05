42a16b18ceb523d2aa6163a2eebbe5600c769517
Flawx <190200856+Matxe24@users.noreply.github.com>
2026-04-05 17:22:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import os
 import subprocess as sub
 import sys
 
-
 if os.name == 'nt':
     print("Sorry, This setup is currently only available on Linux. Try using WSL.")
     print("\nIf you are on windows, check out the README.md file or go to the Github page: https://github.com/Matxe24/tesint")
@@ -13,12 +12,25 @@ else:
     PATH = input("Enter the path in which you want to put Tesint in: ")
     if os.access(PATH, os.W_OK):
         try:
-            sub.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
+            sub.run(
+                [sys.executable, "-m", "pip", "install", "--user", "-r", "requirements.txt"],
+                check=True,
+                stdout=sub.DEVNULL,
+                stderr=sub.DEVNULL
+            )
         except Exception:
-            sub.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt", "--break-system-packages"], check=True)
-        sub.run(["mv", "tesint.py", PATH])
-        newp = f'{PATH}/tesint.py'
-        sub.run('python3', newp)
+            sub.run(
+                [sys.executable, "-m", "pip", "install", "--user", "-r", "requirements.txt", "--break-system-packages"],
+                check=True,
+                stdout=sub.DEVNULL,
+                stderr=sub.DEVNULL
+            )
+
+        target_path = os.path.join(PATH, "tesint.py")
+        os.rename("tesint.py", target_path)
+        os.chmod(target_path, 0o755)
+        sub.run([sys.executable, target_path])
+
     else:
         print("No write permission. Try running the script with sudo.")
         input()
